56b44cd321b6e0b4771dd46e07ab646ef79fba06
AeeUX <andreaseriksen99@hotmail.com>
2021-06-04 03:12:44 +0200

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -8,9 +8,117 @@ import {
     Image
 } from 'react-native'
 
-import { images, icons, COLORS, SIZES } from '../constants';
+import { images, icons, COLORS, SIZES, FONTS } from '../constants';
 
 const Home = () => {
+
+    const [tabList, setTabList] = React.useState([
+        {
+            id: 0,
+            name: "Chair",
+            title: "chairs",
+            productList: [
+                {
+                    productId: 1,
+                    productName: 'Chair Green Colour',
+                    price: 10.00,
+                    image: images.greenChair,
+                },
+                {
+                    productId: 2,
+                    productName: 'Chair Peach Colour',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 3,
+                    productName: 'Chair White Colour',
+                    price: 10.00,
+                    image: images.whiteChair,
+                },
+            ]
+        },
+        {
+            id: 1,
+            name: "Cupboard",
+            title: 'cupboards',
+            productList: [
+                {
+                    productId: 4,
+                    productName: 'Product 4',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 5,
+                    productName: 'Product 5',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 6,
+                    productName: 'Product 6',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+
+            ]
+        },
+        {
+            id: 2,
+            name: "Table",
+            title: 'tables',
+            productList: [
+                {
+                    productId: 7,
+                    productName: 'Product 7',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 8,
+                    productName: 'Product 8',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 9,
+                    productName: 'Product 9',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+
+            ]
+        },
+        {
+            id: 3,
+            name: "Accessories",
+            title: 'accessories',
+            productList: [
+                {
+                    productId: 10,
+                    productName: 'Product 10',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 11,
+                    productName: 'Product 11',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 12,
+                    productName: 'Product 12',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+
+            ]
+        }
+    ])
+
+    
 
     // Render
 
@@ -50,11 +158,20 @@ const Home = () => {
         )
     }
 
+    function renderTitle(title) {
+        return(
+            <View style={{ marginTop: SIZES.padding, marginHorizontal: SIZES.padding }}>
+                <Text style={{ color: COLORS.black, ...FONTS.largeTitle }}>Collection of</Text>
+                <Text style={{ color: COLORS.black, ...FONTS.largeTitle }}>{title}</Text>
+            </View>
+        )
+    }
+
     return(
         <SafeAreaView style={styles.container}>
             {renderHeader()}
 
-            
+            {renderTitle("Test title")}
         </SafeAreaView>
     )
 }
