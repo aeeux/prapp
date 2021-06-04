51d8bad4d982b403af2428ca312ac988a67cb053
AeeUX <andreaseriksen99@hotmail.com>
2021-06-05 01:15:16 +0200

    Update images.js

--- constants/images.js
+++ constants/images.js
@@ -2,10 +2,12 @@ export const sofa = require("../assets/images/sofa.png");
 export const redChair = require("../assets/images/red_chair.png");
 export const greenChair = require("../assets/images/green_chair.png");
 export const whiteChair = require("../assets/images/white_chair.png");
+export const mercedes = require("../assets/images/mercedes.jpg");
 
 export default {
     sofa,
     redChair,
     greenChair,
     whiteChair,
+    mercedes,
 }
