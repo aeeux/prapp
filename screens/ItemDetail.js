3da9c673213cc428c945eb89cf82ebc2338d1fe7
AeeUX <andreaseriksen99@hotmail.com>
2021-06-04 02:47:27 +0200

    Update ItemDetail.js

---
+++ screens/ItemDetail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Text
+} from 'react-native'
+
+const ItemDetail = () => {
+    return(
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default ItemDetail;
