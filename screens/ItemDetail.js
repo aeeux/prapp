b4fe4c48dd6f8dd79eedfa45f9ce90ff0a6adae9
AeeUX <andreaseriksen99@hotmail.com>
2021-06-04 05:36:55 +0200

    Update ItemDetail.js

--- screens/ItemDetail.js
+++ screens/ItemDetail.js
@@ -7,7 +7,7 @@ import {
     ImageBackground,
     TouchableOpacity
 } from 'react-native'
-import { COLORS, icons, SIZES } from '../constants';
+import { COLORS, FONTS, icons, SIZES } from '../constants';
 
 const ItemDetail = ({ route, navigation }) => {
 
@@ -72,9 +72,68 @@ const ItemDetail = ({ route, navigation }) => {
                     {renderHeader()}
 
                     {/* Product Tag */}
-                    <View>
-                        
+                    <View
+                        style={{
+                            position: 'absolute',
+                            top: '45%',
+                            left: '15%',
+                            borderRadius: 80,
+                            backgroundColor: COLORS.transparentLightGray1,
+                            height: 40,
+                            width: 40,
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }}
+                    >
+                        <View
+                            style={{
+                                //top: 15,
+                                borderRadius: 20,
+                                backgroundColor: COLORS.blue,
+                                height: 10,
+                                width: 10
+                            }}
+                        >
+                        </View>
                     </View>
+
+                        <View
+                            style={{
+                                position: 'absolute',
+                                top: '43%',
+                                left: '30%',
+                                flexDirection: 'row',
+                                padding: SIZES.radius * 1.5,
+                                backgroundColor: COLORS.transparentLightGray1,
+                                width: '50%',
+                                borderRadius: 10
+                            }}
+                        >
+                            <View style={{ flex: 2 }}>
+                                <Text style={{ color: COLORS.darkGray, ...FONTS.h3 }}>
+                                    {itemInfo.productName}
+                                </Text>
+                            </View>
+
+                            <View style={{ flex: 1.5, alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+                                <Text style={{ color: COLORS.darkGreen, ...FONTS.h3 }}>
+                                    $ {itemInfo.price.toFixed(2)}
+                                </Text>
+                            </View>
+                        </View>
+
+                        <View
+                            style={{
+                                position: 'absolute',
+                                bottom: "20%",
+                                left: SIZES.padding,
+                                right: SIZES.padding,
+                            }}
+                        >
+                            <Text style={{ color: COLORS.lightGray2, ...FONTS.body2 }}>Cars</Text>
+                            <Text style={{ marginTop: SIZES.radius, color: COLORS.white, ...FONTS.h1 }}>{itemInfo.productName}</Text>
+                        </View>
+
                 </ImageBackground>
             )
         } else {
@@ -82,9 +141,38 @@ const ItemDetail = ({ route, navigation }) => {
         }
     }
 
+    function renderFooter() {
+        return (
+
+        <View
+            style={{
+                position: 'absolute',
+                bottom: '5%',
+                left: SIZES.padding,
+                right: SIZES.padding,
+                flexDirection: 'row',
+                height: 70,
+                backgroundColor: COLORS.white,
+                borderRadius: 35
+            }}
+        >
+            <View style={{ flex: 2, alignitems: 'center', justifyContent: 'center' }}>
+                
+            </View>
+            <View style={{ flex: 2, alignitems: 'center', justifyContent: 'center' }}>
+
+            </View>
+            <View style={{ flex: 2, alignitems: 'center', justifyContent: 'center' }}>
+
+            </View>
+        </View>
+        )
+    }
+
     return(
         <View style={{ flex: 1, backgroundColor: COLORS.white }}>
             {renderInfo()}
+            {renderFooter()}
         </View>
     )
 }
