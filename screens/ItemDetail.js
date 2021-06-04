91b507358bfcdcde0b84b4912e8acad80f1c21d5
AeeUX <andreaseriksen99@hotmail.com>
2021-06-04 05:12:25 +0200

    Update ItemDetail.js

--- screens/ItemDetail.js
+++ screens/ItemDetail.js
@@ -2,13 +2,89 @@ import React from 'react';
 import {
     StyleSheet,
     View,
-    Text
+    Text,
+    Image,
+    ImageBackground,
+    TouchableOpacity
 } from 'react-native'
+import { COLORS, icons, SIZES } from '../constants';
 
-const ItemDetail = () => {
+const ItemDetail = ({ route, navigation }) => {
+
+    // Render
+
+    function renderHeader() {
+        return (
+            <View
+                style={{ 
+                    marginTop: SIZES.padding * 2,
+                    marginHorizontal: SIZES.padding
+                }}
+            >
+                <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity
+                        style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'center' }}
+                        onPress={()=> console.log('Menu-on clicked')}
+                    >
+                        <Image
+                            source={icons.menu}
+                            resizeMode="contain"
+                            style={{
+                                tintColor: COLORS.white,
+                                width: 25,
+                                height: 25
+                            }}
+                        />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        style={{ flex: 1, alignItems: 'flex-end', justifyContent: 'center' }}
+                        onPress={()=> console.log('Search-on clicked')}
+                    >
+                        <Image
+                            source={icons.search}
+                            resizeMode="contain"
+                            style={{
+                                tintColor: COLORS.white,
+                                width: 25,
+                                height: 25
+                            }}
+                        />
+                    </TouchableOpacity>
+
+                </View>
+
+            </View>
+        )
+    }
+
+    function renderInfo() {
+        let { itemInfo } = route.params;
+
+        if(itemInfo) {
+
+            return (
+                <ImageBackground 
+                    source={itemInfo.image}
+                    resizeMode="cover"
+                    style={{ width: '100%', height: '100%' }}
+                >
+                    {renderHeader()}
+
+                    {/* Product Tag */}
+                    <View>
+                        
+                    </View>
+                </ImageBackground>
+            )
+        } else {
+            <View></View>
+        }
+    }
+
     return(
-        <View>
-            <Text>Home</Text>
+        <View style={{ flex: 1, backgroundColor: COLORS.white }}>
+            {renderInfo()}
         </View>
     )
 }
