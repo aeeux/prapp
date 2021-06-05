96fa47c822f63ac0789fe12c34cb8d3db6824e7a
AeeUX <andreaseriksen99@hotmail.com>
2021-06-05 19:44:14 +0200

    Create Slider.js

---
+++ components/Slider.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  useWindowDimensions,
+  StyleSheet,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  pagination: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: -15,
+    alignSelf: 'center',
+  },
+  dot: {
+    color: '#888',
+    fontSize: 50,
+  },
+  activeDot: {
+    color: '#FFF',
+    fontSize: 50,
+  },
+});
+export default function ImageSwipe({img}) {
+  const width = useWindowDimensions().width;
+  const height = width * 0.6;
+
+  const [active, setActive] = useState(0);
+
+  const images = [
+    'https://i.pinimg.com/236x/09/66/4f/09664f3441de659f26bf604a2f1f8f43.jpg',
+    'https://i.pinimg.com/236x/1f/9a/44/1f9a4405c1db5d23a13d8608dfba6850.jpg',
+    'https://i.pinimg.com/236x/28/b9/43/28b94382c8bea2d56afbabe1369d3b68.jpg',
+    'https://i.pinimg.com/564x/9c/40/ac/9c40acb5931b72b8ace4cb446ee0d068.jpg',
+    'https://i.pinimg.com/236x/0d/a7/3b/0da73b6592ba04b63385c12280d1bf6a.jpg',
+  ];
+
+  const change = ({nativeEvent}) => {
+    const slide = Math.ceil(
+      nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width,
+    );
+    if (slide !== active) {
+      setActive(slide);
+    }
+  };
+  return (
+    <View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        onScroll={change}
+        showHorizontalScrollIndicator={false}
+        style={{width, height}}>
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            source={{uri: image}}
+            style={{width, height, resizeMode: 'cover'}}
+          />
+        ))}
+      </ScrollView>
+      <View style={styles.pagination}>
+        {images.map((i, k) => (
+          <Text key={k} style={k == active ? styles.activeDot : styles.dot}>
+            •
+          </Text>
+        ))}
+      </View>
+    </View>
+  );
+}
