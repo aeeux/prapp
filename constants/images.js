fef2b311a6a9bccf70f656b8215a3b97ef0eac59
AeeUX <andreaseriksen99@hotmail.com>
2021-06-05 03:50:45 +0200

    Update images.js

--- constants/images.js
+++ constants/images.js
@@ -1,13 +1,27 @@
-export const sofa = require("../assets/images/sofa.png");
-export const redChair = require("../assets/images/red_chair.png");
-export const greenChair = require("../assets/images/green_chair.png");
-export const whiteChair = require("../assets/images/white_chair.png");
-export const mercedes = require("../assets/images/mercedes.jpg");
+
+//Mercedes
+export const mercedesGlb = require("../assets/images/mercedesGlb.jpg");
+
+//Audi
+export const audiQ8 = require("../assets/images/audiQ8.jpg");
+
+//Lamborghini
+export const lamboUrus = require("../assets/images/lamboUrus.jpg");
+
+//BMW
+export const bmwI8 = require("../assets/images/bmwI8.jpg");
 
 export default {
-    sofa,
-    redChair,
-    greenChair,
-    whiteChair,
-    mercedes,
+
+    //Mercedes
+    mercedesGlb,
+
+    //Audi
+    audiQ8,
+
+    //Lamborghini
+    lamboUrus,
+
+    //BMW
+    bmwI8,
 }
