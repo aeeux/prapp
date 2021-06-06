c7cb7bc69b9c1ec651cb0011f3556aff311fddf2
AeeUX <andreaseriksen99@hotmail.com>
2021-06-06 16:01:41 +0200

    Update images.js

--- constants/images.js
+++ constants/images.js
@@ -9,18 +9,54 @@ export const audiQ8 = require("../assets/images/audiQ8.jpg");
 export const lamboUrus = require("../assets/images/lamboUrus.jpg");
 
 //BMW
-export const bmwI8 = require("../assets/images/bmwI8.jpg");
 export const bmwX6 = require("../assets/images/bmwX6.jpg");
 
-const carouselMercedes = [
+//Carousel-BMW
+const carouselBmwX6 = [
     'https://www.carpixel.net/w/cf38a01edab002e770d0a919b877bd91/bmw-x6-m-competition-car-wallpaper-95619.jpg',
     'https://www.carpixel.net/w/ddb5ff80d081b1a63a964f72e614e54b/bmw-x6-m-competition-car-wallpaper-95628.jpg',
 ];
 
+//Carousel-Mercedes
+const carouselMercedes = [
+    'https://www.carpixel.net/w/a61b0ad3dba8bf6ef498505b9a8cf9c0/mercedes-benz-glb-class-amg-styling-car-wallpaper-97164.jpg',
+    'https://www.carpixel.net/w/9d6f64b6b439a30d9eb50f2804578316/mercedes-benz-glb-class-amg-styling-car-wallpaper-97166.jpg',
+];
+
+//Carousel-Audi
+const carouselAudiQ8 = [
+    'https://www.carpixel.net/w/0d66ad0de8e4de089dd4819ca293a552/audi-rs-q8-car-wallpaper-96714.jpg',
+    'https://www.carpixel.net/w/52f030564a7cd5b8d7e04f3b9f44c522/audi-rs-q8-car-wallpaper-96712.jpg',
+];
+
+//Carousel-Lamborghini
+const carouselLamboUrus = [
+    'https://www.carpixel.net/w/c495f173cdb1c467f5a42d463873c138/lamborghini-urus-car-wallpaper-75923.jpg',
+    'https://www.carpixel.net/w/2140ea2636a388d3a585da51e2c5e069/lamborghini-urus-car-wallpaper-74975.jpg',
+];
+
 export default {
 
-    //Carousels
+
+    //CAROUSELS
+
+
+    //Carousel-BMW
+    carouselBmwX6,
+
+    //Carousel-Mercedes
     carouselMercedes,
+
+    //Carousel-Mercedes
+    carouselAudiQ8,
+
+    //Carousel-Lamborghini
+    carouselLamboUrus,
+
+
+
+    //CARS
+
 
     //Mercedes
     mercedesGlb,
@@ -32,6 +68,5 @@ export default {
     lamboUrus,
 
     //BMW
-    bmwI8,
     bmwX6,
 }
