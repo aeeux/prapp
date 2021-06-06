6dc9c8eac52c3b16f7ca120243129c347bb2f6bf
AeeUX <andreaseriksen99@hotmail.com>
2021-06-06 12:42:45 +0200

    Update images.js

--- constants/images.js
+++ constants/images.js
@@ -12,7 +12,15 @@ export const lamboUrus = require("../assets/images/lamboUrus.jpg");
 export const bmwI8 = require("../assets/images/bmwI8.jpg");
 export const bmwX6 = require("../assets/images/bmwX6.jpg");
 
+const carouselMercedes = [
+    'https://www.carpixel.net/w/cf38a01edab002e770d0a919b877bd91/bmw-x6-m-competition-car-wallpaper-95619.jpg',
+    'https://www.carpixel.net/w/ddb5ff80d081b1a63a964f72e614e54b/bmw-x6-m-competition-car-wallpaper-95628.jpg',
+];
+
 export default {
+
+    //Carousels
+    carouselMercedes,
 
     //Mercedes
     mercedesGlb,
